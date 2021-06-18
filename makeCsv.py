1cd3bff813f808f8877b514e9b3531be6f4e0334
chldyddnjs <48711155+chldyddnjs@users.noreply.github.com>
2021-06-18 20:33:50 +0900

    Add files via upload

--- makeCsv.py
+++ makeCsv.py
@@ -1,14 +1,14 @@
-from numpy.core.numeric import False_
 import pandas as pd
-
+import os
 catagories = ["HeadX","HeadY", "NeckX","NeckY", "RShoulderX","RShoulderY", "RElbowX","RElbowY", "RWristX","RWristY",
                 "LShoulderX","LShoulderY", "LElbowX","LElbowY", "LWristX","LWristY", "RHipX","RHipY", "RKneeX","RKneeY",
-                "RAnkleX","RAnkleY", "LHipX","LHipY", "LKneeX","LKneeY", "LAnkleX","LAnkleY", "ChestX","ChestY"]
-input = [168, 8, 176, 56, 144, 64, 120, 96, 136, 56.1, 216, 72, 224, 120.1, 232, 160, 160.1, 152, 168.1, 232.1, 184, 304, 208, 152.1, 184.1, 224.1, 200, 304.1, 184.2, 112]
-data = dict()
-for i in range(0,30):
-    data[catagories[i]] = int(input[i])
-df = pd.DataFrame([data],columns=["HeadX","HeadY", "NeckX","NeckY", "RShoulderX","RShoulderY", "RElbowX","RElbowY", "RWristX","RWristY",
-                "LShoulderX","LShoulderY", "LElbowX","LElbowY", "LWristX","LWristY", "RHipX","RHipY", "RKneeX","RKneeY",
-                "RAnkleX","RAnkleY", "LHipX","LHipY", "LKneeX","LKneeY", "LAnkleX","LAnkleY", "ChestX","ChestY"])
-df.to_csv('pos.csv',header=False,index=False)
+                "RAnkleX","RAnkleY", "LHipX","LHipY", "LKneeX","LKneeY", "LAnkleX","LAnkleY", "ChestX","ChestY","classID"]
+
+df = pd.DataFrame(columns=[catagories])
+
+if not os.path.exists('pos.csv'):
+    df.to_csv('pos.csv', index=False, mode='w', encoding='utf-8-sig')
+    print('Success')
+else:
+    df.to_csv('pos.csv', index=False, mode='a', encoding='utf-8-sig', header=False)
+    print('Success')
